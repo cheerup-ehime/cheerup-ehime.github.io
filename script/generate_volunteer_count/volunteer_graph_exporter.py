7af3ed39a86082829d853279243fe841198be9cd
Takeshi Kakeda <kakeda@zensow.jp>
2018-07-23 22:35:31 +0900

    週次グラフも追加するようにした

--- script/generate_volunteer_count/volunteer_graph_exporter.py
+++ script/generate_volunteer_count/volunteer_graph_exporter.py
@@ -47,10 +47,17 @@ def load_data_from_gspread():
     df = pd.DataFrame(data=data, columns=cols_name)
     return df
 
+# ## ８日以内のデータのみフィルタする
+def filter_within_week(df):
+    today = dt.datetime.now().strftime('%Y/%m/%d')
+    d_range =  pd.date_range(end=today, periods=8)
+    return df.loc[d_range]
+
 # ## Excelデータを元にグラフを出力
 # df = pd.read_excel('assets/data/ehime_volunteer.xlsx')
 
-def generate_graph(df):
+
+def generate_graph_within_week(df):
     df.replace('', 0, inplace=True)
     df.fillna(0, inplace=True)
 
@@ -58,20 +65,20 @@ def generate_graph(df):
     df2 = df.set_index('Date')
     df2.index.names = ['Date']
 
+    
+    df2 = filter_within_week(df2)
     df2.index = df2.index.strftime("%m/%d")
     df2 = df2.applymap(int)
-    df2.describe()
-
     ax = df2.plot(
                             kind='bar', 
                             figsize=(16,10), 
                             alpha=0.5,
-                            title="愛媛県ボランティア数動向（7月）", 
+                            title="愛媛県ボランティア数動向（一週間）", 
                             subplots=False, 
                             stacked=True,
                             fontsize=20)
 
-    plt.xlabel("月日", fontsize="20")
+    plt.xlabel("日付", fontsize="20")
     plt.ylabel("人数", fontsize="20")
     plt.legend(fontsize="20")
     fig = ax.get_figure()
@@ -109,22 +116,23 @@ def generage_week_graph(df):
     plt.legend(df_w.index.strftime("%m/%d週"),
                     fontsize=18)
     fig.savefig('./assets/images/volunteer_count_week.png')
+    return df_w
 
 # In[37]:
 
 def generate_table(df):
     from tabulate import tabulate
 
-    df_table = df.set_index('Date')
-    df_table.index = df_table.index.strftime('%m/%d')
-    df_table.index.name = '日付'
+    # df_table = df.set_index('Date')
+    # df_table.index = df_table.index.strftime('%m/%d')
+    df.index.name = '日付'
 
-    table = tabulate(df_table, tablefmt="pipe", headers="keys", showindex=True)
+    table = tabulate(df, tablefmt="pipe", headers="keys", showindex=True)
     return table
 
 # In[38]:
 
-def write_article(table):
+def write_article(table_d, table_w):
     from datetime import datetime as dt
     with open('./script/generate_volunteer_count/template.md') as f:
         template = f.read()
@@ -132,7 +140,10 @@ def write_article(table):
         month_day = dt.now().strftime('%m/%d')
         timestamp = dt.now().strftime("%Y-%m-%d %H:%M:%S")
         
-        report = template.format(month_day=month_day, timestamp=timestamp, table=table)
+        report = template.format(month_day=month_day, 
+                        timestamp=timestamp, 
+                        table_d=table_d, 
+                        table_w=table_w)
         
         
     with open('./_pages/volunteer_aggregation.md', 'w') as f:
@@ -141,8 +152,9 @@ def write_article(table):
 
 if __name__ == '__main__':
     df = load_data_from_gspread()
-    generate_graph(df)
-    generage_week_graph(df)
-    table = generate_table(df)
-    write_article(table)
+    df_d = generate_graph_within_week(df)
+    df_w = generage_week_graph(df)
+    table_d = generate_table(df_d)
+    table_w = generate_table(df_w)
+    write_article(table_d, table_w)
 
