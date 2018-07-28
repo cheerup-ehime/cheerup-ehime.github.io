c28f973802af14f71335f67fc47f603dca155584
Satoshi Watanabe <sassy.watanabe@gmail.com>
2018-07-28 16:24:19 +0900

    被害状況を取得するスクリプトを作成

---
+++ script/get_hinanjo_content.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+# pip install pdfminer.six が必要です。
+
+import urllib.request
+import datetime
+
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams, LTRect, LTTextBoxHorizontal
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import PDFPageAggregator
+from io import StringIO
+
+def generate_url():
+    today = datetime.date.today()
+    begin = datetime.date(2018, 6, 1)
+    days = (today - begin).days
+    if (datetime.datetime.now().hour < 12):
+        days -= 1
+
+    return 'http://www.pref.ehime.jp/h12200/documents/higaijokyo' + str(days) + '.pdf'
+
+def get_higaijokyo_content():
+    url = generate_url()
+    rsrcmgr = PDFResourceManager()
+
+    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    content = urllib.request.urlopen(req)
+    with open('data','wb') as output:
+        output.write(content.read())
+    fp = open('data', 'rb')
+    laparams = LAParams()
+    device =  PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+    for page in PDFPage.get_pages(fp, pagenos=None, maxpages=0, password=None,caching=True, check_extractable=True):
+        interpreter.process_page(page)
+        layout = device.get_result()
+        prev_node = None
+        str_line = ''
+        for node in layout:
+            if isinstance(node, LTTextBoxHorizontal):
+                if (prev_node and prev_node.y1 == node.y1):
+                    str_line += ','
+                    str_line += node.get_text().strip()
+                else:
+                    print(str_line)
+                    str_line = node.get_text().strip()
+                prev_node = node
+
+    fp.close()
+    device.close()
+
+if __name__ == '__main__':
+    get_higaijokyo_content()
